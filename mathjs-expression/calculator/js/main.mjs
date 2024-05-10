b4021b8b6c0362b4cd6653233b26d0e1941167cd
Drazen Zadravec <drazenzadravec@users.noreply.github.com>
2024-05-10 11:21:06 +1000

    Create main.mjs

---
+++ mathjs-expression/calculator/js/main.mjs
@@ -0,0 +1,91 @@
+// main
+
+// async fetch.
+var asyncFetch = new AsyncFetch();
+
+/**
+* get the json request.
+*
+* @param {string}	url   the url.
+* @param {Function}	resultAction   the result function. (data: any) => void
+* @param {Function}	errorAction   the error function. (error: any) => void
+*/
+export function getJsonRequest(url, resultAction, errorAction) {
+
+	// get the report data.
+	asyncFetch.jsonRequest(url,
+		{
+			method: 'GET'
+		},
+		function (data) {
+			// send data complete.
+			resultAction(data);
+		},
+		function (error) {
+			// send error
+			errorAction(error);
+		}
+	);
+}
+
+/**
+* post the json request.
+*
+* @param {string}	url   the url.
+* @param {string}	body   the body.
+* @param {Function}	resultAction   the result function. (data: any) => void
+* @param {Function}	errorAction   the error function. (error: any) => void
+*/
+export function postJsonRequest(url, body, resultAction, errorAction) {
+
+	// get the report data.
+	asyncFetch.jsonRequest(url,
+		{
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: body
+		},
+		function (data) {
+			// send data complete.
+			resultAction(data);
+		},
+		function (error) {
+			// send error
+			errorAction(error);
+		}
+	);
+}
+
+/**
+* post the json request.
+*
+* @param {string}	url   the url.
+* @param {string}	body   the body.
+* @param {string}	auth   the authorization header e.g. Bearer [access token].
+* @param {Function}	resultAction   the result function. (data: any) => void
+* @param {Function}	errorAction   the error function. (error: any) => void
+*/
+export function postJsonRequestAuth(url, body, auth, resultAction, errorAction) {
+
+	// get the report data.
+	asyncFetch.jsonRequest(url,
+		{
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json",
+				"Authorization": auth
+			},
+			body: body
+		},
+		function (data) {
+			// send data complete.
+			resultAction(data);
+		},
+		function (error) {
+			// send error
+			errorAction(error);
+		}
+	);
+}
