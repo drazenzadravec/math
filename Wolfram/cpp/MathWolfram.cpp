cf1c56bf54670acd9bde16e47edb80d1233d383d
Drazen Zadravec <drazenzadravec@users.noreply.github.com>
2024-05-22 15:10:27 +1000

    added c++ code

---
+++ Wolfram/cpp/MathWolfram.cpp
@@ -0,0 +1,211 @@
+// MathWolfram.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+#include "stdafx.h"
+
+/// <summary>
+/// Input loop and evaluator
+/// </summary>
+/// <param name="argc"></param>
+/// <param name="argv"></param>
+/// <returns></returns>
+int main(int argc, char* argv[])
+{
+	// if arguments exist.
+	if (argc > 1)
+	{
+		// open confg file
+		std::string line;
+		std::ifstream appConfig("app.config");
+		if (appConfig.is_open())
+		{
+			// get first line
+			std::getline(appConfig, line);
+			appConfig.close();
+		}
+		else
+		{
+			std::cout << "Unable to open file";
+			return 1;
+		}
+
+		// create the command
+		std::string commandLine = "-linkmode launch -linkname \"" + line + "\"";
+
+		WSENV env;
+		WSLINK link;
+		int error;
+
+		// WSTP Initialise
+		env = WSInitialize((WSEnvironmentParameter)0);
+		if (env == (WSENV)0)
+		{
+			std::cout << "Unable to initialize";
+			return 1;
+		}
+
+		// WSTP Open a connection to the wolfram kernel
+		link = WSOpenString(env, commandLine.c_str(), &error);
+		if (link == (WSLINK)0 || error != WSEOK)
+		{
+			std::cout << "Unable to create link";
+			return 1;
+		}
+
+		// WSTP Activate link
+		if (!WSActivate(link))
+		{
+			std::cout << "Link not activated!";
+			return 1;
+		}
+
+		// the expression
+		LLU::WSStream<LLU::WS::Encoding::UTF8, LLU::WS::Encoding::UTF8> expression(link);
+
+		// until result.
+		while (true)
+		{
+			// get input name
+			std::string inputNameString;
+			LLU::WS::Function inputPacketFunction;
+			expression >> inputPacketFunction;
+
+			// get current header name
+			std::string head = inputPacketFunction.getHead();
+			int argCount = inputPacketFunction.getArgc();
+
+			// new line
+			std::cout << std::endl;
+
+			// if return the exit
+			if (head == "ReturnTextPacket")
+			{
+				// new line
+				std::cout << std::endl;
+			}
+
+			if (argCount < 10)
+			{
+				// for each arg count
+				for (int i = 0; i < argCount; i++)
+				{
+					// write the name string.
+					expression >> inputNameString;
+					std::cout << inputNameString;
+				}
+			}
+
+			// if output
+			if (head == "OutputNamePacket"){}
+
+			// if return then exit
+			if (head == "ReturnTextPacket")
+			{
+				// exit loop
+				break;
+			}
+
+			// if input add the argument
+			if (head == "InputNamePacket")
+			{
+				LLU::WS::Function enterTextFunction("EnterTextPacket", 1);
+				expression << enterTextFunction << argv[1];
+			}
+
+			// if error
+			if (head == "MessagePacket")
+			{
+				// exit loop
+				break;
+			}
+		}
+
+		// close.
+		WSClose(link);
+		WSDeinitialize(env);
+	}
+	else
+	{
+		// must have arguments
+		std::cout << "Add arguments";
+	}
+	return 0;
+}
+
+///// <summary>
+///// Input loop and evaluator
+///// </summary>
+///// <param name="argc"></param>
+///// <param name="argv"></param>
+///// <returns></returns>
+//int main(int argc, char* argv[])
+//{
+//	WSENV env;
+//	WSLINK link;
+//	int error;
+//
+//	// WSTP Initialise
+//	env = WSInitialize((WSEnvironmentParameter)0);
+//	if (env == (WSENV)0)
+//	{
+//		std::cout << "Unable to initialize";
+//		return 1;
+//	}
+//
+//	// WSTP Open a connection to the wolfram kernel
+//	link = WSOpenString(env, "-linkmode launch \"C:\\Program Files\\Wolfram Research\\Wolfram Engine\\13.0\\MathKernel.exe\"", &error);
+//	if (link == (WSLINK)0 || error != WSEOK)
+//	{
+//		std::cout << "Unable to create link";
+//		return 1;
+//	}
+//
+//	// WSTP Activate link
+//	if (!WSActivate(link))
+//	{
+//		std::cout << "Link not activated!";
+//		return 1;
+//	}
+//
+//	// Data exchange -->> MyProblem
+//	std::cout << "Create the stream object" << std::endl;
+//	LLU::WSStream<LLU::WS::Encoding::UTF8, LLU::WS::Encoding::UTF8> test(link);
+//
+//	while (true)
+//	{
+//		// Wait for the prompt In[1]:=
+//
+//		std::string inputNameString;
+//		LLU::WS::Function inputPacketFunction;
+//		test >> inputPacketFunction;
+//		std::string Head = inputPacketFunction.getHead();
+//		int ArgCount = inputPacketFunction.getArgc();
+//		std::cout << "-->> Function head:" << Head << " Arguments :" << ArgCount << std::endl;
+//		if (ArgCount < 10)
+//		{
+//			for (int i = 0; i < ArgCount; i++)
+//			{
+//				test >> inputNameString;
+//				std::cout << inputNameString;
+//			}
+//		}
+//		if (Head == "InputNamePacket")
+//		{
+//			std::string inputTextString;
+//			std::cin >> inputTextString;
+//			LLU::WS::Function enterTextFunction("EnterTextPacket", 1);
+//			test << enterTextFunction << inputTextString;
+//
+//		}
+//
+//	}
+//
+//	std::cout << "Close the connection..." << std::endl;
+//
+//	WSClose(link);
+//	WSDeinitialize(env);
+//	return 0;
+//}
---
+++ Wolfram/cpp/stdafx.h
@@ -0,0 +1,34 @@
+#pragma once
+
+#include <windows.h>
+
+#include <math.h>
+#include <cctype>
+#include <cmath>
+#include <cfloat>
+
+#include <stdio.h>
+#include <time.h>
+#include <ctime>
+#include <stdlib.h>
+#include <cstdlib>
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include <algorithm>
+#include <iterator>
+#include <stack>
+#include <type_traits>
+#include <utility>
+
+#include <complex>
+#include <ccomplex>
+#include <memory>
+#include <exception>
+
+#include <functional>
+
+#include "wstp.h"
+#include <LLU/LLU.h>
